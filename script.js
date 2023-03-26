be2921cf9e985ee6bb9758addcea69f16a1e0ff9
ReeVool <126878419+ReeVool@users.noreply.github.com>
2023-03-26 22:08:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var header = document.getElementsByTagName('header')[0];
+
+        window.onscroll = function (event) {
+            var scroll = window.pageYOffset;
+            if (scroll < 100) {
+                header.style.backgroundColor = 'transparent';
+                header.style.boxShadow = 'none';
+                header.style.color = '#fff';
+                }
+
+            else if(scroll >= 100) {
+                header.style.backgroundColor = '#fff';
+                header.style.boxShadow = '0px 20px 20px rgba(0, 0, 0, 0.05)';
+                header.style.color = '#000';
+            }
+        }
