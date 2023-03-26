4a6f012787681200a4d06ed826e2f8f67498527a
ReeVool <126878419+ReeVool@users.noreply.github.com>
2023-03-26 22:12:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ var header = document.getElementsByTagName('header')[0];
             if (scroll < 100) {
                 header.style.backgroundColor = 'transparent';
                 header.style.boxShadow = 'none';
-                header.style.color = '#fff';
                 }
 
             else if(scroll >= 100) {
                 header.style.backgroundColor = '#fff';
                 header.style.boxShadow = '0px 20px 20px rgba(0, 0, 0, 0.05)';
-                header.style.color = '#000';
             }
         }
